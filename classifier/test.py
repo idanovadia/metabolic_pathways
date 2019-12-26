90aaf13a660885ca7c3e0d9eed9e491f5d454939
idan <idanov@post.bgu.ac.il>
2019-12-26 22:48:46 +0200

    creating setup and exec for sub2vec/random walk. create random walk  class with funcs

--- classifier/test.py
+++ classifier/test.py
@@ -13,10 +13,17 @@
 # corr_obj.saveFile()
 
 
-import classifier.graph_creator.graph_creator as gc
+# import classifier.graph_creator.graph_creator as gc
+#
+# gc_obj = gc.Graph_Creator(corr_matrix_path="C:\\Users\\idanf\\PycharmProjects\\metabolic_pathways_new\\classifier"
+#                                            "\\data\\CorrMaxtrix\\CorrMatrix_out_2001_fruit_pericarp_metabolite.xlsx",
+#                           subGraphs_dir_path= "C:\\Users\\idanf\\PycharmProjects\\metabolic_pathways_new\\classifier"
+#                                               "\\data\\labeled_data\\2001\\trainset")
+# gc_obj.create()
 
-gc_obj = gc.Graph_Creator(corr_matrix_path="C:\\Users\\idanf\\PycharmProjects\\metabolic_pathways_new\\classifier"
-                                           "\\data\\CorrMaxtrix\\CorrMatrix_out_2001_fruit_pericarp_metabolite.xlsx",
-                          subGraphs_dir_path= "C:\\Users\\idanf\\PycharmProjects\\metabolic_pathways_new\\classifier"
-                                              "\\data\\labeled_data\\2001\\trainset")
-gc_obj.create()
+import classifier.sub2vec.sub2vec as s2v
+import networkx as nx
+G = nx.Graph()
+s2v_obj = s2v.Sub2Vec()
+G.add_edges_from([("a", "e"), ("a", "c"), ("a", "d"), ("c", "b"), ("b", "e"), ("e", "d")])
+rw_graph = s2v_obj.randomWalk(G)
