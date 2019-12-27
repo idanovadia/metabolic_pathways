ff59d7e2833bbd8775b6fd60947b5a5ec3567c80
idan <idanov@post.bgu.ac.il>
2019-12-27 14:51:26 +0200

    create doc2vec class

--- classifier/test.py
+++ classifier/test.py
@@ -20,10 +20,15 @@
 #                           subGraphs_dir_path= "C:\\Users\\idanf\\PycharmProjects\\metabolic_pathways_new\\classifier"
 #                                               "\\data\\labeled_data\\2001\\trainset")
 # gc_obj.create()
+#
+# import classifier.sub2vec.sub2vec as s2v
+# import networkx as nx
+# G = nx.Graph()
+# s2v_obj = s2v.Sub2Vec()
+# G.add_edges_from([("a", "e"), ("a", "c"), ("a", "d"), ("c", "b"), ("b", "e"), ("e", "d")])
+# rw_graph = s2v_obj.randomWalk(G)
+import classifier.sub2vec.doc2vec as d2v
 
-import classifier.sub2vec.sub2vec as s2v
-import networkx as nx
-G = nx.Graph()
-s2v_obj = s2v.Sub2Vec()
-G.add_edges_from([("a", "e"), ("a", "c"), ("a", "d"), ("c", "b"), ("b", "e"), ("e", "d")])
-rw_graph = s2v_obj.randomWalk(G)
+a = [["a","b","c","d"],["b","k","r","t"],["c","d","f","e"],["e","k","r","y"],["g","h","c","d"]]
+d2v_obj = d2v.Doc2Vec(a)
+d2v_obj.Doc2Vec()
