b8e10a71635c2393fc3f5db88cc3271e330d0e26
idan <idanov@post.bgu.ac.il>
2019-12-27 15:11:04 +0200

    generate sub graphs in sub2vec - reading all sub graphs

--- classifier/sub2vec/sub2vec.py
+++ classifier/sub2vec/sub2vec.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from random import randint
+import networks as nx
+import os
 
 from classifier.code_tools.Abstract_config_class import AbstractConfigClass
 
@@ -13,10 +15,16 @@ class Sub2Vec(AbstractConfigClass):
     def setup(self):
         self.randomWalk_threshold = self.config_parser.eval(self.__class__.__name__, 'random_walk_threshold')
         self.random_walk_graphs_to_create = self.config_parser.eval(self.__class__.__name__, 'random_walk_graphs_to_create')
+        self.subGraphs_directory_path = self.getPath(relative_path=self.config_parser.eval(self.__class__.__name__, 'subGraphs_directory_path'))
+        self.subGraphs_list = []
         pass
 
     def exec(self):
         pass
 
+    # use directory path and read all the saved sab graphs there
+    def generateSubGraphs(self):
+        for filename in os.listdir(self.subGraphs_directory_path):
+            self.subGraphs_list.append(nx.read_gml(os.path.join(self.subGraphs_directory_path,filename)))
 
 
