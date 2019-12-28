fdbdfd171135935fe3faf62d1a7598a2fca26f4b
idan <idanov@post.bgu.ac.il>
2019-12-28 22:32:41 +0200

    fix random walk - get sub graphs as nx files and return random walk graphs as nx files fix ini and sub2vec

--- classifier/sub2vec/sub2vec.py
+++ classifier/sub2vec/sub2vec.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 from random import randint
-import networks as nx
+import networkx as nx
 import os
-
+import classifier.sub2vec.randonWalk as rw
 from classifier.code_tools.Abstract_config_class import AbstractConfigClass
 
 
@@ -12,15 +12,25 @@ class Sub2Vec(AbstractConfigClass):
     def __init__(self):
         AbstractConfigClass.__init__(self)
 
+    '''
+    randomWalk_threshold - number of nodes in each graph
+    random_walk_graphs_to_create - number of graphs to create of each sub graph
+    subGraphs_directory_path - directory path to sub graphs files (networks format)
+    '''
     def setup(self):
         self.randomWalk_threshold = self.config_parser.eval(self.__class__.__name__, 'random_walk_threshold')
         self.random_walk_graphs_to_create = self.config_parser.eval(self.__class__.__name__, 'random_walk_graphs_to_create')
         self.subGraphs_directory_path = self.getPath(relative_path=self.config_parser.eval(self.__class__.__name__, 'subGraphs_directory_path'))
         self.subGraphs_list = []
+        self.list_of_graphs = []
         pass
 
     def exec(self):
-        pass
+        self.generateSubGraphs()
+        random_walk_object = rw.RandomWalk(threshold=self.randomWalk_threshold, number_of_graphs=self.random_walk_graphs_to_create)
+        for g in self.subGraphs_list:
+            for i in range(self.random_walk_graphs_to_create):
+                list_of_graphs = random_walk_object.insertGraphToSet(list_of_graphs=list_of_graphs, graph=random_walk_object.randomWalk(g))
 
     # use directory path and read all the saved sab graphs there
     def generateSubGraphs(self):
--- classifier/test.py
+++ classifier/test.py
@@ -22,13 +22,32 @@
 # gc_obj.create()
 #
 # import classifier.sub2vec.sub2vec as s2v
-# import networkx as nx
+import networkx as nx
 # G = nx.Graph()
 # s2v_obj = s2v.Sub2Vec()
 # G.add_edges_from([("a", "e"), ("a", "c"), ("a", "d"), ("c", "b"), ("b", "e"), ("e", "d")])
 # rw_graph = s2v_obj.randomWalk(G)
-import classifier.sub2vec.doc2vec as d2v
+# import classifier.sub2vec.doc2vec as d2v
+#
+# a = [["a","b","c","d"],["b","k","r","t"],["c","d","f","e"],["e","k","r","y"],["g","h","c","d"]]
+# d2v_obj = d2v.Doc2Vec(a)
+# d2v_obj.Doc2Vec()
 
-a = [["a","b","c","d"],["b","k","r","t"],["c","d","f","e"],["e","k","r","y"],["g","h","c","d"]]
-d2v_obj = d2v.Doc2Vec(a)
-d2v_obj.Doc2Vec()
+G1 = nx.fast_gnp_random_graph(20, 0.5)
+G2 = nx.fast_gnp_random_graph(20, 0.3)
+G3 = nx.fast_gnp_random_graph(20, 0.7)
+G4 = nx.fast_gnp_random_graph(20, 0.8)
+G5 = nx.fast_gnp_random_graph(20, 0.5)
+G6 = nx.fast_gnp_random_graph(20, 0.6)
+
+path = "C:\\Users\\idanf\\PycharmProjects\\metabolic_pathways_new\\classifier\data\\test_random_walk_files\\"
+
+nx.write_gml(G1, path + "test1.gml.gz")
+nx.write_gml(G2, path + "test2.gml.gz")
+nx.write_gml(G3, path + "test3.gml.gz")
+nx.write_gml(G4, path + "test4.gml.gz")
+nx.write_gml(G5, path + "test5.gml.gz")
+nx.write_gml(G6, path + "test6.gml.gz")
+
+
+
