9ff875c1f5fc47e874b07cbc34ae84cb5129e6f5
eran.saranga@gmail.com <eran.saranga@gmail.com>
2020-03-11 16:23:58 +0200

    Added Validation Wrappers

---
+++ classifier/Validation/Wrappers/KFoldWrapper.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import KFold
+from ..ValidationMethodAbstract import ValidationMethodAbstract
+
+
+class KFoldWrapper(ValidationMethodAbstract):
+
+    def __init__(self,x_train,y_train,**kwargs):
+        self.x_train=x_train
+        self.y_train=y_train
+        self.splits=kwargs['kfold_split']
+        self.name='KFold'
+
+
+    def split(self):
+        kf= KFold(self.splits, shuffle=True)
+        for train_index, test_index in kf.split(self.x_train):
+            x_train, x_test = self.x_train[train_index], self.x_train[test_index]
+            y_train, y_test = self.y_train[train_index], self.y_train[test_index]
+            yield x_train , y_train , x_test ,y_test
+
+
+
+
+
+
+
+
+
+
+
---
+++ classifier/Validation/Wrappers/LeaveOneOutWrapper.py
@@ -0,0 +1,28 @@
+from sklearn.model_selection import LeaveOneOut
+from ..ValidationMethodAbstract import ValidationMethodAbstract
+
+
+class LeaveOneOutWrapper(ValidationMethodAbstract):
+
+    def __init__(self,x_train,y_train):
+        self.x_train=x_train
+        self.y_train=y_train
+
+    def split(self):
+        loo = LeaveOneOut()
+        loo.get_n_splits(self.x_train)
+        for train_index, test_index in loo.split(self.data):
+            x_train, x_test = self.x_train.iloc[train_index, :], self.x_train.iloc[test_index, :]
+            y_train, y_test = self.y_train[train_index], self.y_train[test_index]
+            yield  x_train , y_train , x_test , y_test
+
+
+
+
+
+
+
+
+
+
+
