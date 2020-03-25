93913191686cefd4d52113ecbee8d18d8505621e
idan <idanov@post.bgu.ac.il>
2020-03-25 16:07:20 +0200

    svm

--- classifier/Classification/Wrappers/catboostWrapper.py
+++ classifier/Classification/Wrappers/catboostWrapper.py
@@ -13,13 +13,13 @@ class CatBoost(ClassifierAbstract):
         self.learning_rate = kwargs['cat_boost_learning_rate']
         self.loss_function = kwargs['cat_boost_loss_function']
         self.name = 'CatBoost'
-        self.model = model = CatBoostClassifier(iterations=self.iterations,
+        self.model = CatBoostClassifier(iterations=self.iterations,
                                                 depth=self.depth,
                                                 learning_rate=self.learning_rate,
                                                 loss_function=self.loss_function,
                                                 verbose=True)
 
-        pass
+
 
     def fit(self, x_train, y_train):
         self.model.fit(x_train, y_train)
