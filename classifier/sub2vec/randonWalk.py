9ed5905ed637af6f3a08e3bd844c83ecdbfc0287
eran.saranga@gmail.com <eran.saranga@gmail.com>
2020-04-27 23:10:47 +0300

    rw that start on labled data nodes and going in main graph path

--- classifier/sub2vec/randonWalk.py
+++ classifier/sub2vec/randonWalk.py
@@ -5,12 +5,12 @@ import random
 
 class RandomWalk():
 
-    def __init__(self, threshold, number_of_graphs,args,extensions):
+    def __init__(self, threshold, number_of_graphs,args,extensions,main_graph):
         self.threshold = threshold
         self.number_of_graphs = number_of_graphs
         self.args=args
         self.extensions=extensions
-
+        self.main_graph=main_graph
 
     # def graphComparator(self, graph_1, graph_2):
     #     if sorted(list(graph_1.nodes)) == sorted(list(graph_2.nodes)):
@@ -22,9 +22,13 @@ class RandomWalk():
         for counter, j in enumerate(graph.nodes):
             for i in range(self.number_of_graphs):
                 if 'rw_on_main_graph' in self.extensions:
-                    new_graph=self.randomWalk_on_main_graph()
-                new_graph = self.randomWalk(graph, j, i)
-                new_graph.graph["name"] = str(i) + "_" + str(counter) + "_" + new_graph.graph["name"]
+                    new_graph=self.randomWalk_on_main_graph(self.main_graph,j)
+                    new_graph.graph["name"] = str(i) + "_" + str(counter) + "_" + graph.graph['name']
+                    new_graph.graph['label']=graph.graph['label']
+                    new_graph.graph['type']=graph.graph['type']
+                else:
+                    new_graph = self.randomWalk(graph, j, i)
+                    new_graph.graph["name"] = str(i) + "_" + str(counter) + "_" + new_graph.graph["name"]
                 rwList.append(new_graph)
         list_of_graphs.append(rwList)
         return list_of_graphs
@@ -32,7 +36,21 @@ class RandomWalk():
         self.empty_nodes_list = False
         rw_nodes_list = []
         count = 0
-        nodes=list(main_graph.node(root))
+        nodes = list(main_graph.nodes)
+        nodes.append(root)
+        count, node = self.getFirstNode(count, nodes, rw_nodes_list, root)
+        while count < self.threshold and len(nodes) > 0:
+            nodes = self.getNeighbors(node, main_graph)
+            if len(nodes) == 0:
+                break
+            if 'weighted_randomwalk' in self.extensions:
+
+                count, node = self.getNextNodeByWeights(count, nodes, rw_nodes_list, self.main_graph, node)
+            else:
+                count, node = self.getNextNode(count, nodes, rw_nodes_list)
+
+        return self.main_graph.subgraph(rw_nodes_list).copy()
+
     # Get sub-graph in nx format
     def randomWalk(self, sub_graph, root, sub_name):
         self.empty_nodes_list = False
@@ -57,6 +75,7 @@ class RandomWalk():
 
 
     def getNextNodeByWeights(self, count, nodes, rw_nodes_list, sub_graph, node):
+        rw_nodes_list.append(node)
         sum_of_weights = self.getSumOfWeights(sub_graph, node)
         random_number = randint(0, sum_of_weights)
         new_node = random.choice(nodes)
@@ -93,7 +112,12 @@ class RandomWalk():
 
 
     def getNeighbors(self, node, sub_graph):
-        return list(sub_graph.adj[node].keys())
+        if node in sub_graph.adj:
+            return list(sub_graph.adj[node].keys())
+        else:
+            return []
+
+
 
     def randomNode(self, nodes_list, left_nodes):
         index = randint(0, len(nodes_list) - 1)
